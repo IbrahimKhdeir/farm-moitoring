dabc55effa439ed30f545eaf261282adf942bf46
amer <4mer.c56@gmail.com>
2025-12-10 14:32:39 +0200

    Add .gitignore file and protect device routes with authentication middleware

--- bake/src/modules/devices/device.route.js
+++ bake/src/modules/devices/device.route.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const validate = require('../../middleware/validate.middleware');
+const authMiddleware = require('../../middleware/auth.middleware');
 const { addDevice, getDevices } = require('./device.controller');
 const { createDeviceSchema } = require('./device.validation');
 
-router.post('/', validate(createDeviceSchema), addDevice);
-router.get('/', getDevices);
+// Protect device routes: require authentication to list/create devices.
+router.post('/', authMiddleware, validate(createDeviceSchema), addDevice);
+router.get('/', authMiddleware, getDevices);
 
 module.exports = router;
